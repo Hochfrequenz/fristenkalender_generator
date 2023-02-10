5130a675e9f62e7cf0639a549df573e342466277
Nicola Söker <nicola.soeker@hochfrequenz.de>
2023-02-10 14:21:00 +0100

    tuple -> fristen object

--- src/fristenkalender_generator/mymodule.py
+++ src/fristenkalender_generator/mymodule.py
@@ -1,10 +1,31 @@
 """
-This a docstring for the module.
+This module can produce a list of calender entries with bdew Fristen
 """
 
 from bdew_datetimes.periods import get_nth_working_day_of_month, get_previous_working_day
 from calendar import monthrange
-from datetime import date
+from datetime import date, datetime
+import dataclasses
+
+
+@dataclasses.dataclass
+class Frist_with_attributes:
+    """
+    This class represents a Frist with its attibutes: date_of_the_frist = date(y,m,d) and type_of_the_frist = '...'
+    """
+
+    date_of_the_frist: date
+    type_of_the_frist: str
+
+
+@dataclasses.dataclass
+class Date_with_all_its_fristen:
+    """
+    This calss represents a day with all its Fristen
+    """
+
+    date_of_the_day: datetime
+    list_of_fristen: list
 
 
 class Fristenkalender_generator:
@@ -17,7 +38,7 @@ class Fristenkalender_generator:
         Initialize for the sake of initializing
         """
 
-    def generate_fristen_liste_variable_WT(self, year: int, nth_day: int, fristen_type: str) -> list:
+    def generate_fristen_list_variable_WT(self, year: int, nth_day: int, fristen_type: str) -> list:
         """
         generate the list of nth_day WT Fristen for a given year
         """
@@ -27,34 +48,34 @@ class Fristenkalender_generator:
         # oct from last year, only if relevant for the current year's calender
         nth_working_day_of_month_date = get_nth_working_day_of_month(nth_day, start=date(year - 1, 10, 1))
         if nth_working_day_of_month_date >= date(year - 1, 12, 1):
-            fristen_list.append((fristen_type, nth_working_day_of_month_date))
+            fristen_list.append(Frist_with_attributes(nth_working_day_of_month_date, fristen_type))
 
         # nov from last year, only if relevant for the current year's calender
         nth_working_day_of_month_date = get_nth_working_day_of_month(nth_day, start=date(year - 1, 11, 1))
         if nth_working_day_of_month_date >= date(year - 1, 12, 1):
-            fristen_list.append((fristen_type, nth_working_day_of_month_date))
+            fristen_list.append(Frist_with_attributes(nth_working_day_of_month_date, fristen_type))
 
         # dez from last year
         nth_working_day_of_month_date = get_nth_working_day_of_month(nth_day, start=date(year - 1, 12, 1))
-        fristen_list.append((fristen_type, nth_working_day_of_month_date))
+        fristen_list.append(Frist_with_attributes(nth_working_day_of_month_date, fristen_type))
 
         # this year
         n_Months = 12
         for i_Month in range(1, n_Months + 1):
             nth_working_day_of_month_date = get_nth_working_day_of_month(nth_day, start=date(year, i_Month, 1))
             if nth_working_day_of_month_date < date(year + 1, 2, 1):
-                fristen_list.append((fristen_type, nth_working_day_of_month_date))
+                fristen_list.append(Frist_with_attributes(nth_working_day_of_month_date, fristen_type))
 
         # jan of next year
         nth_working_day_of_month_date = get_nth_working_day_of_month(
             nth_day, start=date(year + 1, 12, 1)
         )  # dez form last year
         if nth_working_day_of_month_date < date(year + 1, 2, 1):
-            fristen_list.append((fristen_type, nth_working_day_of_month_date))
+            fristen_list.append(Frist_with_attributes(nth_working_day_of_month_date, fristen_type))
 
         return fristen_list
 
-    def generate_fristen_liste_LWT(self, year: int) -> list:
+    def generate_fristen_list_LWT(self, year: int) -> list:
         """
         generate the list of LWT Fristen for a given year
         """
@@ -65,25 +86,25 @@ class Fristenkalender_generator:
         # dez last year
         last_day_of_Month = monthrange(year, 12)[1]
         last_date_of_Month = date(year - 1, 12, last_day_of_Month)
-        fristen_list.append((fristen_type, get_previous_working_day(last_date_of_Month)))
+        fristen_list.append(Frist_with_attributes(get_previous_working_day(last_date_of_Month), fristen_type))
 
         # this year
         n_Months = 12
         for i_Month in range(1, n_Months + 1):
             last_day_of_Month = monthrange(year, i_Month)[1]
             last_date_of_Month = date(year - 1, 12, last_day_of_Month)
-            fristen_list.append((fristen_type, get_previous_working_day(last_date_of_Month)))
+            fristen_list.append(Frist_with_attributes(get_previous_working_day(last_date_of_Month), fristen_type))
 
         # jan next year
         last_day_of_Month = monthrange(year, 1)[1]
         last_date_of_Month = date(year + 1, 1, last_day_of_Month)
-        fristen_list.append((fristen_type, get_previous_working_day(last_date_of_Month)))
+        fristen_list.append(Frist_with_attributes(get_previous_working_day(last_date_of_Month), fristen_type))
 
         return fristen_list
 
-    def generate_fristen_liste_3LWT(self, year: int) -> list:
+    def generate_fristen_list_3LWT(self, year: int) -> list:
         """
-        generate the list of LWT Fristen for a given year
+        generate the list of 3LWT Fristen for a given year
         """
 
         fristen_type = "3LWT"
@@ -94,67 +115,45 @@ class Fristenkalender_generator:
         date_dummy = get_previous_working_day(date(year - 1, 12, last_day_of_Month))
         while last_day_of_Month - date_dummy.day < 3:
             date_dummy = get_previous_working_day(date_dummy)
-        fristen_list.append((fristen_type, date_dummy))
+        fristen_list.append(Frist_with_attributes(date_dummy, fristen_type))
 
         # this year
         n_Months = 12
         for i_Month in range(1, n_Months + 1):
             last_day_of_Month = monthrange(year, i_Month)[1]
             last_date_of_Month = date(year, i_Month, last_day_of_Month)
-            date_dummy = date(year, i_Month, last_day_of_Month)
-            date_dummy = get_previous_working_day(date_dummy)
+            date_dummy = get_previous_working_day(date(year, i_Month, last_day_of_Month))
             while last_day_of_Month - date_dummy.day < 3:
                 date_dummy = get_previous_working_day(date_dummy)
-            fristen_list.append((fristen_type, date_dummy))
+            fristen_list.append(Frist_with_attributes(date_dummy, fristen_type))
 
         # jan next year
         last_day_of_Month = monthrange(year + 1, 1)[1]
-        date_dummy = date(year + 1, 1, last_day_of_Month)
-        date_dummy = get_previous_working_day(date_dummy)
+        date_dummy = get_previous_working_day(date(year + 1, 1, last_day_of_Month))
         while last_day_of_Month - date_dummy.day < 3:
             date_dummy = get_previous_working_day(date_dummy)
-        fristen_list.append((fristen_type, date_dummy))
+        fristen_list.append(Frist_with_attributes(date_dummy, fristen_type))
 
         return fristen_list
 
-    def generate_fristen_df(self, year: int) -> list:
+    def generate_all_fristen_list(self, year: int) -> list:
         """
         generate the list of all Fristen for a given year
         """
         fristen_list = []
-        fristen_list.append(generate_fristen_liste_variable_WT(year, 5, "5WT"))
-        fristen_list.append(generate_fristen_liste_variable_WT(year, 10, "10WT"))
-        fristen_list.append(generate_fristen_liste_variable_WT(year, 12, "12WT"))
-        fristen_list.append(generate_fristen_liste_variable_WT(year, 14, "14WT"))
-        fristen_list.append(generate_fristen_liste_variable_WT(year, 16, "16WT"))
-        fristen_list.append(generate_fristen_liste_variable_WT(year, 17, "17WT"))
-        fristen_list.append(generate_fristen_liste_variable_WT(year, 18, "18WT"))
-        fristen_list.append(generate_fristen_liste_variable_WT(year, 20, "20WT"))
-        fristen_list.append(generate_fristen_liste_variable_WT(year, 21, "21WT"))
-        fristen_list.append(generate_fristen_liste_variable_WT(year, 26, "26WT"))
-        fristen_list.append(generate_fristen_liste_variable_WT(year, 30, "30WT"))
-        fristen_list.append(generate_fristen_liste_variable_WT(year, 42, "42WT"))
-        fristen_list.append(generate_fristen_liste_LWT(year))
-        fristen_list.append(generate_fristen_liste_3LWT(year))
+        fristen_list.extend(self.generate_fristen_list_variable_WT(year, 5, "5WT"))
+        fristen_list.extend(self.generate_fristen_list_variable_WT(year, 10, "10WT"))
+        fristen_list.extend(self.generate_fristen_list_variable_WT(year, 12, "12WT"))
+        fristen_list.extend(self.generate_fristen_list_variable_WT(year, 14, "14WT"))
+        fristen_list.extend(self.generate_fristen_list_variable_WT(year, 16, "16WT"))
+        fristen_list.extend(self.generate_fristen_list_variable_WT(year, 17, "17WT"))
+        fristen_list.extend(self.generate_fristen_list_variable_WT(year, 18, "18WT"))
+        fristen_list.extend(self.generate_fristen_list_variable_WT(year, 20, "20WT"))
+        fristen_list.extend(self.generate_fristen_list_variable_WT(year, 21, "21WT"))
+        fristen_list.extend(self.generate_fristen_list_variable_WT(year, 26, "26WT"))
+        fristen_list.extend(self.generate_fristen_list_variable_WT(year, 30, "30WT"))
+        fristen_list.extend(self.generate_fristen_list_variable_WT(year, 42, "42WT"))
+        fristen_list.extend(self.generate_fristen_list_LWT(year))
+        fristen_list.extend(self.generate_fristen_list_3LWT(year))
 
         return fristen_list
-
-
-class MyClass:  # pylint: disable=too-few-public-methods
-    """
-    This is a docstring for the class.
-    """
-
-    def __init__(self):
-        """
-        Initialize for the sake of initializing
-        """
-        self.my_instance_var: str = "abc"
-
-    def do_something(self) -> str:
-        """
-        Actually does nothing.
-        :return: the value of an instance variable
-        """
-        # this is a super long line with: 100 < line length <= 120 to demonstrate the purpose of pyproject.toml
-        return self.my_instance_var
--- unittests/test_myclass.py
+++ unittests/test_myclass.py
@@ -1,8 +1,7 @@
-from fristenkalender_generator.mymodule import MyClass
-
 from fristenkalender_generator.mymodule import Fristenkalender_generator
 
 from datetime import date
+
 
 class TestFristenkalender_generator:
     """
@@ -10,35 +9,38 @@ class TestFristenkalender_generator:
     """
 
     def test_if_first_10WT_day_is_right(self):
-        assert Fristenkalender_generator().generate_fristen_liste_variable_WT(2023, 10, '10WT')[0][1] == date(2022,12,14)
+        assert Fristenkalender_generator().generate_fristen_list_variable_WT(2023, 10, "10WT")[
+            0
+        ].date_of_the_frist == date(2022, 12, 14)
 
     def test_if_first_5WT_day_is_right(self):
-        assert Fristenkalender_generator().generate_fristen_liste_variable_WT(2023, 5,'5WT')[0][1] == date(2022,12,7)
+        assert Fristenkalender_generator().generate_fristen_list_variable_WT(2023, 5, "5WT")[
+            0
+        ].date_of_the_frist == date(2022, 12, 7)
 
     def test_if_first_12WT_day_is_right(self):
-        assert Fristenkalender_generator().generate_fristen_liste_variable_WT(2023, 12,'12WT')[0][1] == date(2022,12,16)
+        assert Fristenkalender_generator().generate_fristen_list_variable_WT(2023, 12, "12WT")[
+            0
+        ].date_of_the_frist == date(2022, 12, 16)
 
-    def test_if_first_12WT_day_is_right(self):
-        assert Fristenkalender_generator().generate_fristen_liste_variable_WT(2023, 14,'14WT')[0][1] == date(2022,12,20)
+    def test_if_first_14WT_day_is_right(self):
+        assert Fristenkalender_generator().generate_fristen_list_variable_WT(2023, 14, "14WT")[
+            0
+        ].date_of_the_frist == date(2022, 12, 20)
 
     def test_if_first_42WT_day_is_right(self):
-        assert Fristenkalender_generator().generate_fristen_liste_variable_WT(2023, 42, '42WT')[0][1] == date(2022, 12, 5)
+        assert Fristenkalender_generator().generate_fristen_list_variable_WT(2023, 42, "42WT")[
+            0
+        ].date_of_the_frist == date(2022, 12, 5)
 
     def test_if_first_LWT_day_is_right(self):
-        assert Fristenkalender_generator().generate_fristen_liste_LWT(2023)[0][1] == date(2022, 12, 30)
+        assert Fristenkalender_generator().generate_fristen_list_LWT(2023)[0].date_of_the_frist == date(2022, 12, 30)
 
     def test_if_first_3LWT_day_is_right(self):
-        assert Fristenkalender_generator().generate_fristen_liste_3LWT(2023)[0][1] == date(2022, 12, 28)
+        assert Fristenkalender_generator().generate_fristen_list_3LWT(2023)[0].date_of_the_frist == date(2022, 12, 28)
 
     def test_if_last_3LWT_day_is_right(self):
-        assert Fristenkalender_generator().generate_fristen_liste_3LWT(2023)[13][1] == date(2024, 1, 26)
+        assert Fristenkalender_generator().generate_fristen_list_3LWT(2023)[-1].date_of_the_frist == date(2024, 1, 26)
 
-
-class TestMyClass:
-    """
-    A class with pytest unit tests.
-    """
-
-    def test_something(self):
-        my_class = MyClass()
-        assert my_class.do_something() == "abc"
+    def test_if_last_frist_in_fristen_calender_is_right(self):
+        assert Fristenkalender_generator().generate_all_fristen_list(2023)[-1].date_of_the_frist
