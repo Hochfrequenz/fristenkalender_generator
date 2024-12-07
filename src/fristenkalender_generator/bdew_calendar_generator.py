80e6a0ca78d822760554a573576d9f6b7abc3654
Konstantin <konstantin.klein+github@hochfrequenz.de>
2024-12-07 14:34:35 +0100

    pylint

--- src/fristenkalender_generator/bdew_calendar_generator.py
+++ src/fristenkalender_generator/bdew_calendar_generator.py
@@ -319,7 +319,8 @@ class FristenkalenderGenerator:
 
         fristen = []
         for days, label in days_and_labels:
-            if label == "LWT" or label == "3LWT":
+            if label == "LWT" or label == "3LWT": # pylint:disable=consider-using-in
+                # ignore pylint because we need the x==FOO or x==BAR for mypy LwtLabel type
                 fristen += self.generate_all_fristen_for_given_lwt(year, days, label)
             elif label.endswith("WT"):
                 fristen += self.generate_all_fristen_for_given_wt(year, days, label)
