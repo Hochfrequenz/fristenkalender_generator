206e72d10279c18fe7681e1dde5eb76512474f38
Konstantin <konstantin.klein+github@hochfrequenz.de>
2025-01-08 16:18:25 +0100

    fix: Uppercase "Sie" anstatt "sie" in deutscher Anrede

--- src/fristenkalender_generator/bdew_calendar_generator.py
+++ src/fristenkalender_generator/bdew_calendar_generator.py
@@ -148,9 +148,9 @@ A dictionary with a specific descriptions of the frists
 
 greeting: str = "Digitaler Hochfrequenz Fristenkalender \n"
 general_description: str = (
-    "\n Um die Kalenderereignisse einfach zu löschen, geben sie \n"
+    "\n Um die Kalenderereignisse einfach zu löschen, geben Sie \n"
     "'Hochfrequenz Fristenkalender' in das Suchfeld ihrer Kalenderapp ein \n"
-    "und bearbeiten sie die Liste nach Wunsch.\n\n"
+    "und bearbeiten Sie die Liste nach Wunsch.\n\n"
     "Hochfrequenz Unternehmensberatung GmbH\n"
     "Nördliche Münchner Straße 27A\n"
     "D-82031 Grünwald\n"
